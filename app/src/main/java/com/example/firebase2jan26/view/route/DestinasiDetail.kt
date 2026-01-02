5447d85e988519eb55ccad8aa0397b86dc6afdcf
Synzenn <velriel42@gmail.com>
2026-01-02 21:33:10 +0700

    Menambahkan destinasi detail untuk navigasi siswa.

---
+++ app/src/main/java/com/example/firebase2jan26/view/route/DestinasiDetail.kt
@@ -0,0 +1,11 @@
+package com.example.firebase2jan26.view.route
+
+
+import com.example.firebase2jan26.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
