135104532130e43099042394d4c9ca368620f9b5
Synzenn <velriel42@gmail.com>
2026-01-02 21:33:31 +0700

    Menambahkan destinasi navigasi untuk layar home.

---
+++ app/src/main/java/com/example/firebase2jan26/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.firebase2jan26.view.route
+
+import com.example.firebase2jan26.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
