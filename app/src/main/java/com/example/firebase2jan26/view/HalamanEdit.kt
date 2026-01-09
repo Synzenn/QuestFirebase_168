d38ffa7f2886e56b156c33580efce35e5c193465
Synzenn <velriel42@gmail.com>
2026-01-09 16:25:52 +0700

    Membuat HalamanEdit untuk tampilan edit.

---
+++ app/src/main/java/com/example/firebase2jan26/view/HalamanEdit.kt
@@ -0,0 +1,14 @@
+package com.example.firebase2jan26.view
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.firebase2jan26.view.route.DestinasiEdit
+import com.example.firebase2jan26.viewmodel.EditViewModel
+import com.example.firebase2jan26.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
