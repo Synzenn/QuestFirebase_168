9ec2f6a3c856771141c9d5cfa4eb4f3c754f5aa4
Synzenn <velriel42@gmail.com>
2026-01-09 16:19:10 +0700

    Menambahkan Card untuk detail data siswa di HalamanDetail.

--- app/src/main/java/com/example/firebase2jan26/view/route/HalamanDetail.kt
+++ app/src/main/java/com/example/firebase2jan26/view/route/HalamanDetail.kt
@@ -143,3 +143,40 @@ fun DetailDataSiswa(
     siswa: Siswa,
     modifier: Modifier = Modifier
 ) {
+    Card(
+        modifier = modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+        ) {
+            BarisDetailData(
+                labelResId = R.string.nama,
+                itemDetail = siswa.nama,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+            BarisDetailData(
+                labelResId = R.string.alamat,
+                itemDetail = siswa.alamat,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+            BarisDetailData(
+                labelResId = R.string.telpon,
+                itemDetail = siswa.telpon,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+        }
+    }
+}
